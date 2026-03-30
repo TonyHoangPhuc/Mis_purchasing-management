6dd8a190a40c5618a9e30f25b5cf4f4634fd63ff
HoangPhuc <tonyphuc9999@gmail.com>
2026-03-30 17:24:43 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-echo "Initializing Odoo database..."
+echo "Starting Odoo..."
 
 odoo \
   -c /etc/odoo/odoo.conf \
   --http-port=$PORT \
-  --proxy-mode \
-  -d misproject_mwvg \
-  -i base \
-  --without-demo=all \
-  --stop-after-init
+  --proxy-mode

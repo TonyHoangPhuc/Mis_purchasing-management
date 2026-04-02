fae6bb2df591aedc43f2bd98ed00bc634097d02a
HoangPhuc <tonyphuc9999@gmail.com>
2026-04-03 00:46:05 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Initializing database if needed..."
+
+odoo \
+  -c /etc/odoo/odoo.conf \
+  -d postgres \
+  -i base \
+  --without-demo=all \
+  --stop-after-init
+
 echo "Starting Odoo..."
 
 odoo \

a5999454d7e33f2f037f58183e4031f4ef14d349
HoangPhuc <tonyphuc9999@gmail.com>
2026-04-03 01:00:41 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-echo "Initializing database..."
-
-odoo \
-  -c /etc/odoo/odoo.conf \
-  -d postgres \
-  -i base \
-  --without-demo=all \
-  --stop-after-init
-
 echo "Starting Odoo..."
 
-exec odoo \
+odoo \
   -c /etc/odoo/odoo.conf \
   --http-port=$PORT \
   --http-interface=0.0.0.0 \

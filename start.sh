0528c71c790468047939876974a497459468c44c
HoangPhuc <tonyphuc9999@gmail.com>
2026-04-05 15:03:26 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-echo "===== INIT ODOO DATABASE ====="
-echo "DB NAME: $DB_NAME"
+echo "STARTING ODOO..."
 
 odoo \
+  -c /etc/odoo/odoo.conf \
   --db_host=$DB_HOST \
   --db_port=$DB_PORT \
   --db_user=$DB_USER \
   --db_password=$DB_PASSWORD \
   -d postgres \
-  -i base \
-  --db_sslmode=require \
+  -i base,web \
+  --without-demo=all \
   --workers=0 \
   --max-cron-threads=0 \
   --http-port=10000 \
-  --http-interface=0.0.0.0
+  --http-interface=0.0.0.0 \
+  --stop-after-init

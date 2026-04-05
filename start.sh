b6ddc17922aac150cf5988b6e08f4fb28867ec57
HoangPhuc <tonyphuc9999@gmail.com>
2026-04-05 13:07:54 +0700

    DB Aiven

--- start.sh
+++ start.sh
@@ -4,9 +4,8 @@ echo "Starting Odoo..."
 
 odoo \
   -c /etc/odoo/odoo.conf \
-  --http-port=10000\
+  --http-port=10000 \
   --http-interface=0.0.0.0 \
   --proxy-mode \
-  --db-filter=^neondb$ \
-  -i base \
+  --db-filter=.* \
   --without-demo=all

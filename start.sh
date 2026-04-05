9767f50c7be2b94738d5b6ef17ef177112efd94d
HoangPhuc <tonyphuc9999@gmail.com>
2026-04-05 13:52:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Starting Odoo..."
+echo "DB NAME IS: $DB_NAME"
+echo "START SCRIPT RUNNING"
 
 odoo \
   --db_host=$DB_HOST \
@@ -11,4 +13,4 @@ odoo \
   --db_sslmode=require \
   --http-port=10000 \
   --http-interface=0.0.0.0 \
-  --proxy-mode
+  --proxy-mode 
